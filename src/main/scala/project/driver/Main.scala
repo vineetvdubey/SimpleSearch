7e5d90c43a6b37721f1e838425b4678448757bfa
Vineet Dubey <vineet@enliteresearch.com>
2019-08-18 22:15:46 +0530

    Removed hardcoded input

--- src/main/scala/project/driver/Main.scala
+++ src/main/scala/project/driver/Main.scala
@@ -5,7 +5,7 @@ import project.simplesearch.SimpleSearch
 object Main extends App {
 
   SimpleSearch
-    .readFile(Array("folder"))
+    .readFile(args)
     .fold(
       println,
       file => SimpleSearch.iterate(SimpleSearch.index(file))
